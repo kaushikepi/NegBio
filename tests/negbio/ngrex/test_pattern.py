ef26de98a5a8ec21034f15c1dd80f1dd84af3edf
Yifan Peng <yifan.peng@nih.gov>
2019-02-28 00:51:04 -0500

    test cases for ngex pattern

--- tests/negbio/ngrex/test_pattern.py
+++ tests/negbio/ngrex/test_pattern.py
@@ -1,5 +1,8 @@
 import networkx as nx
 from negbio import ngrex
+import pytest
+
+from negbio.ngrex.pattern import validate_names
 
 
 def get_graph():
@@ -13,10 +16,8 @@ def get_graph():
     return G
 
 
-def helper(G, p, expected):
+def _helper(G, p, expected):
     pattern = ngrex.compile(p)
-    print(pattern.pattern)
-    # actual = {m.group(0) for m in pattern.finditer(G)}
     actual = set()
     for m in pattern.finditer(G):
         actual.add(m.group(0))
@@ -25,52 +26,65 @@ def helper(G, p, expected):
 
 def test_regex():
     G = get_graph()
-    helper(G, '{} >{dependency:/aaa|bbb/} {}', {'xxx', 'yyy'})
+    _helper(G, '{} >{dependency:/aaa|bbb/} {}', {'xxx', 'yyy'})
 
 
 def test_attribute():
     G = get_graph()
-    helper(G, '{} >{dependency:/aaa|bbb/} {}', {'xxx', 'yyy'})
-    helper(G, '{} >{tag:/aaa|bbb/} {}', set())
+    _helper(G, '{} >{dependency:/aaa|bbb/} {}', {'xxx', 'yyy'})
+    _helper(G, '{} >{tag:/aaa|bbb/} {}', set())
 
 
 def test_relation():
     G = get_graph()
-    helper(G, '{lemma:/xxx/} >{dependency:/aaa/} {lemma:/yyy/}', {'xxx'})
-    helper(G, '{lemma:/yyy/} <{dependency:/aaa/} {lemma:/xxx/}', {'yyy'})
-    helper(G, '{} >{} {}', {'xxx', 'yyy'})
+    _helper(G, '{lemma:/xxx/} >{dependency:/aaa/} {lemma:/yyy/}', {'xxx'})
+    _helper(G, '{lemma:/yyy/} <{dependency:/aaa/} {lemma:/xxx/}', {'yyy'})
+    _helper(G, '{} >{} {}', {'xxx', 'yyy'})
 
 
 def test_relation_next():
     G = get_graph()
-    helper(G, '{lemma:/xxx/} >{dependency:/aaa/} ({lemma:/yyy/} >{dependency:/bbb/} {lemma:/zzz/})',
-           {'xxx'})
+    _helper(G,
+           '{lemma:/xxx/} >{dependency:/aaa/} ({lemma:/yyy/} >{dependency:/bbb/} {lemma:/zzz/})',
+            {'xxx'})
 
 
 def test_relation_conj():
     G = get_graph()
-    helper(G, '{} >{} {lemma:/yyy/} >{} {lemma:/zzz/}', {'xxx'})
-    helper(G, '{} >{} {lemma:/yyy/} <{} {lemma:/zzz/}', set())
+    _helper(G, '{} >{} {lemma:/yyy/} >{} {lemma:/zzz/}', {'xxx'})
+    _helper(G, '{} >{} {lemma:/yyy/} <{} {lemma:/zzz/}', set())
 
 
 def test_relation_disj():
     G = get_graph()
-    helper(G, '{} >{dependency:/aaa/} {} | >{dependency:/bbb/} {}', {'xxx', 'yyy'})
+    _helper(G, '{} >{dependency:/aaa/} {} | >{dependency:/bbb/} {}', {'xxx', 'yyy'})
 
 
-def test_variables():
+def test_names():
     G = get_graph()
     pattern = ngrex.compile('{}=t >{dependency:/aaa|bbb/} {}')
-    print(pattern.pattern)
     actual = {m.get('t') for m in pattern.finditer(G)}
     assert actual == {'xxx', 'yyy'}
 
+    with pytest.raises(KeyError):
+        pattern = ngrex.compile('{}=t >{dependency:/aaa|bbb/} {}')
+        m = next(pattern.finditer(G))
+        m.get('x')
 
-if __name__ == '__main__':
-    # test_relation()
-    # test_relation_next()
-    test_relation_conj()
-    # test_relation_disj()
-    # test_regex()
-    # test_attribute()
-    # test_variables()
+
+def test_MatcherObj():
+    G = get_graph()
+    pattern = ngrex.compile('{lemma:/xxx/} >{dependency:/aaa/} {lemma:/yyy/}')
+    matcher = next(pattern.finditer(G))
+    assert bool(matcher)
+    assert matcher.graph == G
+    assert matcher.pattern == pattern
+    groups = list(matcher.groups())
+    assert len(groups) == 2
+    assert groups[0] == 'xxx'
+    assert groups[1] == 'yyy'
+
+
+def test_validate_names():
+    with pytest.raises(KeyError):
+        ngrex.compile('{}=t >{dependency:/aaa|bbb/} {}=t')

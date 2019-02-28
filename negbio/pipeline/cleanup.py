c7975378ff49d8154155b4bb8560bdc07329c794
Yifan Peng <yifan.peng@nih.gov>
2019-02-28 01:14:36 -0500

    test cases for cleanup

--- negbio/pipeline/cleanup.py
+++ negbio/pipeline/cleanup.py
@@ -1,6 +1,3 @@
-import logging
-
-
 def clean_sentences(document, sort_anns=False):
     """
     Remove sentences in each passage
@@ -9,17 +6,15 @@ def clean_sentences(document, sort_anns=False):
         document(BioCDocument): a document
         sort_anns(bool): sort ann by its location
     """
-    try:
-        for passage in document.passages:
-            del passage.sentences[:]
+    for passage in document.passages:
+        del passage.sentences[:]
 
-        if sort_anns:
-            key_func = lambda ann: ann.get_total_location().offset
-            id = 0
-            for passage in document.passages:
-                for ann in sorted(passage.annotations, key=key_func):
-                    ann.id = str(id)
-                    id += 1
-    except:
-        logging.exception("Cannot process %s", document.id)
+    if sort_anns:
+        key_func = lambda ann: ann.total_span.offset
+        id = 0
+        for passage in document.passages:
+            passage.annotations = sorted(passage.annotations, key=key_func)
+            for ann in passage.annotations:
+                ann.id = str(id)
+                id += 1
     return document

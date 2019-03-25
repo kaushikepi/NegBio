88d5c8153db4a8ee699257c76a9bb9e6777052f6
Yifan Peng <yifan.peng@nih.gov>
2019-03-25 10:00:11 -0400

    add file lock

--- negbio/pipeline/scan.py
+++ negbio/pipeline/scan.py
@@ -30,15 +30,29 @@ def scan_document(*_, **kwargs):
     verbose = kwargs.pop('verbose', True)
     directory = os.path.expanduser(kwargs.pop('directory'))
     suffix = kwargs.pop('suffix')
+    skip_exists = kwargs.pop('skip_exists', False)
     fn = kwargs.pop('fn')
     non_sequences = kwargs.pop('non_sequences', [])
 
     if not os.path.exists(directory):
-        os.makedirs(directory)
+        os.makedirs(directory, exist_ok=True)
 
-    for pathname in tqdm.tqdm(source, total=len(source), disable=not verbose):
+    for pathname in tqdm.tqdm(source, total=len(source), disable=not verbose, unit='collection'):
         stem = os.path.splitext(os.path.basename(pathname))[0]
         dstname = os.path.join(directory, '{}{}'.format(stem, suffix))
+
+        # add file lock
+        if skip_exists:
+            if os.path.exists(dstname):
+                continue
+
+        lckname = dstname + '.lck'
+        if os.path.exists(lckname):
+            continue
+
+        with open(lckname, 'w') as _:
+            pass
+
         try:
             with io.open(pathname, encoding='utf8') as fp:
                 collection = bioc.load(fp)
@@ -47,13 +61,15 @@ def scan_document(*_, **kwargs):
             continue
 
         new_documents = []
-        for document in collection.documents:
+        for document in tqdm.tqdm(collection.documents, unit='doc', disable=not verbose, leave=False):
             try:
                 document = fn(document, *non_sequences)
                 if document is None or not isinstance(document, bioc.BioCDocument):
                     raise TypeError('The function need to return a document: %s' % fn)
             except TypeError as e:
                 raise e
+            except SystemExit:
+                exit(1)
             except:
                 logging.exception('Cannot process %s', document.id)
             new_documents.append(document)
@@ -64,6 +80,8 @@ def scan_document(*_, **kwargs):
                 bioc.dump(collection, fp)
         except:
             logging.exception('Cannot write %s', pathname)
+        finally:
+            os.remove(lckname)
 
 
 def scan_collection(*_, **kwargs):
